82b1b88ee0bbc7dbd7e85ca2feb7871cb4e25503
IrinaZhuravel <irenasuarchive@gmail.com>
2025-04-01 20:51:03 +0300

    color-switcher (setInterval)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+
+let timerId = null;
+
+const startColorSwitch = () => {
+  if (timerId) return;
+  startBtn.setAttribute("disabled", "true");
+  stopBtn.removeAttribute("disabled");
+
+  timerId = setInterval(() => {
+    const bgc = getRandomHexColor();
+    body.style.backgroundColor = bgc;
+  }, 1000);
+};
+
+const stopColorSwitch = () => {
+clearInterval(timerId);
+startBtn.removeAttribute("disabled");
+stopBtn.setAttribute("disabled", "true");
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener("click", startColorSwitch);
+stopBtn.addEventListener('click', stopColorSwitch);
 
